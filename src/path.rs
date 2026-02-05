9cf90d97a397a1d2844fdb0f586c7670ca47abd3
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-02-05 13:54:40 +0800

    refactor: use the `if cfg!` syntax

--- src/path.rs
+++ src/path.rs
@@ -1,35 +1,24 @@
-use std::path::PathBuf;
+use dirs::{data_local_dir, runtime_dir};
+use std::{env::var, path::PathBuf};
 
 pub fn user_uds_path() -> anyhow::Result<PathBuf> {
-    #[cfg(unix)]
-    {
-        use dirs::{data_local_dir, runtime_dir};
-
-        let base = runtime_dir().or_else(data_local_dir).unwrap();
-
-        let path = base.join("diamond.sock");
-        Ok(path)
-    }
-    #[cfg(windows)]
-    {
-        use std::env::var;
-
+    if cfg!(windows) {
         let user = var("USERNAME")?;
         let path = PathBuf::from("\\\\.\\pipe")
             .join("diamond-user")
             .join(user)
             .join("api.sock");
-        Ok(path)
+        return Ok(path);
     }
+
+    let base = runtime_dir().or_else(data_local_dir).unwrap();
+    let path = base.join("diamond.sock");
+    Ok(path)
 }
 
 pub fn system_uds_path() -> PathBuf {
-    #[cfg(unix)]
-    {
-        "/var/run/diamond.sock".into()
+    if cfg!(windows) {
+        return "\\\\.\\pipe\\diamond-system\\api.sock".into();
     }
-    #[cfg(windows)]
-    {
-        "\\\\.\\pipe\\diamond-system\\api.sock".into()
-    }
+    "/var/run/diamond.sock".into()
 }
