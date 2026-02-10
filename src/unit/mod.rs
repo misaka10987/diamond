3905f0904fd2f170ab1789cbd880d637b4c349b1
misaka10987 <136697326+misaka10987@users.noreply.github.com>
2026-02-10 15:29:07 +0800

    feat: sending input to child process

--- src/unit/mod.rs
+++ src/unit/mod.rs
@@ -1,8 +1,10 @@
 use std::{fs::File, process::Stdio, sync::OnceLock};
 
+use anyhow::anyhow;
 use serde::{Deserialize, Serialize};
 use serde_inline_default::serde_inline_default;
 use tokio::{
+    io::AsyncWriteExt,
     process::{Child, Command},
     sync::Mutex,
 };
@@ -65,6 +67,24 @@ impl Unit {
             .await
             .set(proc)
             .expect("starting a unit for multiple times");
+        Ok(())
+    }
+
+    /// Send an input to the process's `stdin`. This immediately flushes the input, so the process will receive it as soon as possible.
+    ///
+    /// Note that the argument `line` will be trimmed, and an LF will be automatically appended to it before sending.
+    /// To write raw bytes directly, use [`Self::input`] instead.
+    pub async fn input_line(&self, line: &str) -> anyhow::Result<()> {
+        let line = format!("{}\n", line.trim());
+        self.input(line.as_bytes()).await
+    }
+
+    pub async fn input(&self, buf: &[u8]) -> anyhow::Result<()> {
+        let mut proc = self.proc.lock().await;
+        let proc = proc.get_mut().ok_or(anyhow!("unit not started"))?;
+        let stdin = proc.stdin.as_mut().ok_or(anyhow!("unit has no stdin"))?;
+        stdin.write_all(buf).await?;
+        stdin.flush().await?;
         Ok(())
     }
 }
